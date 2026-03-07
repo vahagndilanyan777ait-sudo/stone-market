4939b1c9302ab0ff03f25af5ee0295fefe4f3707
vahagndilanyan777ait-sudo <vahagn.dilanyan777.ait@gmail.com>
2026-03-07 12:48:46 +0400

    Create Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
